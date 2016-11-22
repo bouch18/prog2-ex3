0e40cd3a6c9ce059f202f3bf00a2c6ccb9c9c5d8
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-11-22 11:47:41 +0900

    Pair Programing Exercise3

---
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/ExMain.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165706;
+
+/**
+ * Created by e165706 on 2016/11/22.
+ */
+public class ExMain {
+
+    public static void main() {
+        jp.ac.uryukyu.ie.e165706.Mize();
+    }
+
+}
