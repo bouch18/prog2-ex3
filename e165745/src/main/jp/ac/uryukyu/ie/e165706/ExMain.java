dcb78b2ad83d4fa01c169e1a51d8377b47cc6cbf
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-11-29 11:42:40 +0900

    Pair Programing Exercise3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/ExMain.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/ExMain.java
@@ -12,7 +12,29 @@ import java.util.Scanner;
 public class ExMain {
 
     public static void main(String[] args) {
+        String filename = "./src/main/jp/ac/uryukyu/ie/e165706/map.txt";
+        List<String> strings = new ArrayList<>();
+        Scanner scanner;
 
+        /* 想定している例外が起きる箇所では、予め例外に対応した処理を記述する。
+            try{ 例外が起きる箇所 }
+            catch(想定してる例外){例外処理}
+            finally{例外の有無にかかわらず、最終的に実行させたい処理}
+         */
+        try {
+            scanner = new Scanner(new File(filename));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                strings.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1); //終了ステータス。ここでは取り敢えず0(=正常)以外にした。
+        }
+
+        String first_place = strings.get(1);
+        System.out.println(first_place);
         //map.txtを読み込んだ迷路インスタンスを生成。
         /*String filename = "./src/main/jp/ac/uryukyu/ie/e165706/map.txt";
         List<String> strings = new ArrayList<>();
--- e165745/src/main/jp/ac/uryukyu/ie/e165706/Maze.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Maze.java
@@ -7,24 +7,27 @@ import java.util.List; // いわゆる「リスト構造」のインターフェ
 import java.util.Scanner; // 標準入力読み込みの際に利用したクラス
 
 
-public  class Maze {
-    System.out.println("ファイル名を入力してください: ");
-    Scanner scan = new Scanner(System.in);
-    String str = scan.next();
+public class Maze {
+
+    public void maze() {
+        System.out.println("ファイル名を入力してください: ");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
 
     /* ファイルのパスは、プロジェクトのトップディレクトリを基準とした相対パスで記述。
     .javaファイルを置いてるディレクトリ基準ではないことに注意。 */
-    String filename = "./src/main/jp/ac/uryukyu/ie/e165706/" +str;
-    List<String> strings = new ArrayList<>(); // ファイルから読み込んだ文字列を保存するためのリスト。
-    Scanner scanner;
+        String filename = "./src/main/jp/ac/uryukyu/ie/e165706/" + str;
+        List<String> strings = new ArrayList<>(); // ファイルから読み込んだ文字列を保存するためのリスト。
+        Scanner scanner;
 
 
-    //map.txtの初期位置にプレイヤーインスタンスを生成。
-    String first_place = strings.get(1);
-    List<int> luke = new ArrayList<>();
-        for(int i=0, i<2, i++){
-        int k = Integer.parseInt(first_place(i));
-        luke.add(int k);
+        //map.txtの初期位置にプレイヤーインスタンスを生成。
+        String first_place = strings.get(1);
+        List<Integer> luke = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            int k = Integer.parseInt(first_place);
+            luke.add(k);
+        }
+
     }
-
 }
