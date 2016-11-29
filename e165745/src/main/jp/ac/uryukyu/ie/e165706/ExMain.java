242d35500e315c234583c207f153f1da4f987bbc
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-11-29 10:03:01 +0900

    Pair Programing Exercise3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/ExMain.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/ExMain.java
@@ -1,19 +1,44 @@
 package jp.ac.uryukyu.ie.e165706;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by e165706 on 2016/11/22.
  */
 public class ExMain {
 
     public static void main() {
-        jp.ac.uryukyu.ie.e165706.Mize();
 
-        jp.ac.uryukyu.ie.e165706
+
 
         //map.txtを読み込んだ迷路インスタンスを生成。
-        read_txt();
+        String filename = "./src/main/jp/ac/uryukyu/ie/e165706/map.txt";
+        List<String> strings = new ArrayList<>();
+        Scanner scanner;
+
+        try {
+            scanner = new Scanner(new File(filename));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                strings.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
         //map.txtの初期位置にプレイヤーインスタンスを生成。
-        ply_first_place();
+        String first_place = String.get(0);
+        List<int> luke = new ArrayList<>();
+        for(int i=0, i<2, i++){
+            int k = Integer.parseInt(first_place(i));
+            luke.add(int k);
+        }
+        //ply_first_place();
         //迷路＋プレイヤー現在位置を表示。
         //プレイヤーインスタンスを左に移動。
         //迷路＋プレイヤー現在位置を表示。
