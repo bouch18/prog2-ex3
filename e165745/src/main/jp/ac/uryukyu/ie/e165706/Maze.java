446e6a1d10ca1ad65ed39d27bf2ed9b590698bc1
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-12-06 09:55:28 +0900

    Pair Programing Exercise3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/Maze.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Maze.java
@@ -7,27 +7,39 @@ import java.util.List; // いわゆる「リスト構造」のインターフェ
 import java.util.Scanner; // 標準入力読み込みの際に利用したクラス
 
 
-public class Maze {
+class Maze {
+    private List<String> strings = new ArrayList<>();
+    private String first_place;
+    private String map_size;
+        /* 想定している例外が起きる箇所では、予め例外に対応した処理を記述する。
+            try{ 例外が起きる箇所 }
+            catch(想定してる例外){例外処理}
+            finally{例外の有無にかかわらず、最終的に実行させたい処理}
+         */
 
-    public void maze() {
-        System.out.println("ファイル名を入力してください: ");
-        Scanner scan = new Scanner(System.in);
-        String str = scan.next();
 
-    /* ファイルのパスは、プロジェクトのトップディレクトリを基準とした相対パスで記述。
-    .javaファイルを置いてるディレクトリ基準ではないことに注意。 */
-        String filename = "./src/main/jp/ac/uryukyu/ie/e165706/" + str;
-        List<String> strings = new ArrayList<>(); // ファイルから読み込んだ文字列を保存するためのリスト。
+    Maze() {
         Scanner scanner;
-
-
-        //map.txtの初期位置にプレイヤーインスタンスを生成。
-        String first_place = strings.get(1);
-        List<Integer> luke = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            int k = Integer.parseInt(first_place);
-            luke.add(k);
+        String filename = "./src/main/jp/ac/uryukyu/ie/e165706/map.txt";
+        try {
+            scanner = new Scanner(new File(filename));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                this.strings.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1); //終了ステータス。ここでは取り敢えず0(=正常)以外にした。
         }
-
+        first_place = this.strings.get(0);
+        map_size = this.strings.get(1);
+        this.strings.remove(0);
+        this.strings.remove(0);
     }
+
+    String getStartPlace(){return first_place;}
+    String getMapSize(){return map_size;}
+    List<String> getmap(){return strings;}
+
 }
