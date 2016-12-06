c52a531280dd93bfc1fdb1cfa0af1b93d141ea02
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-12-06 09:53:47 +0900

    Pair Programing Exercise3

---
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165706;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Maze maze = new Maze();
+        Player player = new Player();
+    }
+
+}
+
