68f3f3495bb76ba5a9bc050d7f93bc97f98bb668
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2016-12-06 10:45:48 +0900

    prog2_ex3

---
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e165706;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Maze maze = new Maze();
+        Player player = new Player(maze.getStartPlace(), maze.getMapSize(), maze.getmap());
+
+        System.out.println(maze.getmap().get(3).substring(0,1));
+        System.out.println(player.getPlayer_x());
+
+    }
+
+}
