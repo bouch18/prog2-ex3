d6964fada5097718bfc65b8c0e0896b07c5e994e
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-12-06 11:01:52 +0900

    Pair Programing Exercise3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/Player.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Player.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 class Player {
 
-        private int player_x;
-        private int player_y;
-        private int map_x;
-        private int map_y;
-        private List<String> take;
+        private int player_x;      //プレイヤーのx座標の値
+        private int player_y;      //プレイヤーのy座標の値
+        private int map_x;    //mapの最大値のx座標
+        private int map_y;    //mapの最大値のy座標
+        private List<String> take;   //Mazeで作ったマップをこの変数に入れる
 
-        Player(String like, String h, List<String> rise){
+        Player(String like, String h, List<String> rise){  //ここでは、上のフィールド変数に適切な値を代入する。
 
             this.take = rise;
             String[] p = like.split("\\s+",0);
@@ -42,32 +42,30 @@ class Player {
             }
 
         }
-        void under_go(int player_x, int player_y) {
+        void under_go(int player_x, int player_y) { //引数にはプレイヤーの現在地をとり、
             String roal = take.get(player_y + 1).substring(player_x, player_x + 1);
             if (roal == " "){
                 this.player_y += 1;
             }
         }
         void right_go(int player_x, int player_y) {
-            String roal = take.get(player_y + 1).substring(player_x, player_x + 1);
+            String roal = take.get(player_y).substring(player_x +1, player_x + 2);
             if (roal == " "){
                this.player_x += 1;
             }
         }
         void left_go(int player_x, int player_y) {
-            String roal = take.get(player_y + 1).substring(player_x, player_x + 1);
+            String roal = take.get(player_y).substring(player_x - 1, player_x);
             if (roal == " "){
             this.player_x -= 1;
             }
         }
         void up_go(int player_x, int player_y) {
-            String roal = take.get(player_y + 1).substring(player_x, player_x + 1);
+            String roal = take.get(player_y - 1).substring(player_x, player_x + 1);
             if (roal == " "){
             this.player_y -= 1;
             }
         }
-    
-
 
     String player_point(int player_x, int player_y){
             String loid = take.get(player_y);
