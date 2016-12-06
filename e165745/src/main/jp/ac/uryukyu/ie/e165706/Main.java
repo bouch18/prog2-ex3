d5ccf57d9970a4242a621882186428e46d17355f
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2016-12-06 11:27:00 +0900

    prog2_ex3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
@@ -3,13 +3,23 @@ package jp.ac.uryukyu.ie.e165706;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         Maze maze = new Maze();
         Player player = new Player(maze.getStartPlace(), maze.getMapSize(), maze.getmap());
+        System.out.println(maze.getmap().get(3).substring(0, 1));
+        System.out.println("プレイヤーの初期位置: " + player.getPlayer_x() + "," + player.getPlayer_y());
+        while ( !=G)
+        if ( =="#"){//left is fall
+            if ( =="#"){//under is fall
+                Player.right_go();
+                if (=="#"){//right is fall
+                    Player.up_go();
+                    Player.under_go();
+                }
+            else{//ahter than that
+                    player.left_go();
+                }
 
-        System.out.println(maze.getmap().get(3).substring(0,1));
-        System.out.println(player.getPlayer_x());
-
+            }
+        }
     }
-
 }
