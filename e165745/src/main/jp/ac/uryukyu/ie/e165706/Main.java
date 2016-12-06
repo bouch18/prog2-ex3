1c8e43905dafbdff20ea3e30b06810eb1b93292a
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2016-12-06 11:48:36 +0900

    prog2_ex3

--- e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
+++ e165745/src/main/jp/ac/uryukyu/ie/e165706/Main.java
@@ -7,18 +7,19 @@ public class Main {
         Player player = new Player(maze.getStartPlace(), maze.getMapSize(), maze.getmap());
         System.out.println(maze.getmap().get(3).substring(0, 1));
         System.out.println("プレイヤーの初期位置: " + player.getPlayer_x() + "," + player.getPlayer_y());
-        while ( !=G)
-        if ( =="#"){//left is fall
-            if ( =="#"){//under is fall
-                Player.right_go();
+        while (Player.player_point() == ture) {
+            if ( =="#"){//left is fall
+                if ( =="#"){//under is fall
+                    Player.right_go();
+                }
+                if (=="#"){//right is fall
+                    Player.up_go();
+                }
+                Player.under_go();
             }
-            if (=="#"){//right is fall
-                Player.up_go();
-            }
-            Player.under_go();
-        }
             else{//ahter than that
-            player.left_go();
+                player.left_go();
+            }
         }
     }
 }
